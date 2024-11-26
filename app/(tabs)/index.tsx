324ccc8974a44a6a84c552cd1f5e23c60a243bb0
Irem Cam <iremcam43@gmail.com>
2024-11-26 17:31:37 +0100

    changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcomeeee!</ThemedText>
+        <ThemedText type="title">Welcomee!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
