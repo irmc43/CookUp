72eead6a5417d114dd0d7e372f5e1f56e284fd3d
irmc43 <167435863+irmc43@users.noreply.github.com>
2025-01-01 14:48:10 +0000

    installed NativeWind

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Button, StyleSheet, ScrollView } from 'react-native';
+import "./global.css"
 import { initializeApp } from '@firebase/app';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from '@firebase/auth';
 
