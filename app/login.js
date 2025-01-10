389d0fab8c2258458cd383695d6bdc66e333e642
rozanalraie <rozanzidansuleiman.alraie@stud.hs-ruhrwest.de>
2025-01-10 17:11:43 +0100

    hinzufügen eines Benutzernamens

--- app/login.js
+++ app/login.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from "react";
 import { View, Text, TextInput, Button, StyleSheet, ScrollView } from "react-native";
 import { useRouter } from "expo-router";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "@firebase/auth";
-import { app } from "./firebase.config";
+import { doc, setDoc } from "firebase/firestore";
+import { firestore, app } from "./firebase.config";
 
 const AuthScreen = ({
   email,
   setEmail,
   password,
   setPassword,
+  username,
+  setUsername,
   isLogin,
   setIsLogin,
   handleAuthentication,
@@ -16,6 +19,18 @@ const AuthScreen = ({
   return (
     <View style={styles.authContainer}>
       <Text style={styles.title}>{isLogin ? "Anmelden" : "Registrieren"}</Text>
+      
+      {!isLogin && (
+        <TextInput
+          style={styles.input}
+          value={username}
+          onChangeText={setUsername}
+          placeholder="Benutzername"
+          autoCapitalize="none"
+          placeholderTextColor="#929292"
+        />
+      )}
+
 
       <TextInput
         style={styles.input}
@@ -53,11 +68,12 @@ const AuthScreen = ({
 export default function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState(""); // Zustand für Benutzernamen
   const [user, setUser] = useState(null);
   const [isLogin, setIsLogin] = useState(true);
   const router = useRouter();
 
-  const auth = getAuth(app); // Verwende die importierte App
+  const auth = getAuth(app);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -77,8 +93,16 @@ export default function App() {
         await signInWithEmailAndPassword(auth, email, password);
         console.log("User signed in successfully!");
       } else {
-        await createUserWithEmailAndPassword(auth, email, password);
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
         console.log("User created successfully!");
+
+        // Speichere den Benutzernamen in Firestore
+        const userId = userCredential.user.uid;
+        await setDoc(doc(firestore, "users", userId), {
+          username,
+          email,
+        });
+        console.log("Username saved successfully!");
       }
     } catch (error) {
       console.error("Authentication error:", error.message);
@@ -93,6 +117,8 @@ export default function App() {
           setEmail={setEmail}
           password={password}
           setPassword={setPassword}
+          username={username}
+          setUsername={setUsername}
           isLogin={isLogin}
           setIsLogin={setIsLogin}
           handleAuthentication={handleAuthentication}
